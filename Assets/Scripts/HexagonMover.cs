7639884146eb9e41f0742bccd1efa7f7d7137015
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-04-11 11:56:24 +0300

    Move Fixed

--- Assets/Scripts/HexagonMover.cs
+++ Assets/Scripts/HexagonMover.cs
@@ -1,39 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class HexagonMover : MonoBehaviour
 {
-    public float moveDuration = 0.25f;
-    public ColorWheelController colorWheelController;
-
+    [SerializeField] private float moveDuration = 0.25f;
+    [SerializeField] private Button enqueuePatternButton;
+    [SerializeField] private Button enqueueSecondPatternButton;
+    [SerializeField] private GameObject startPatternSprite;
+    
     private bool isMoving = false;
-    
-    private Queue<Vector3> moveCommands = new Queue<Vector3>();
-    
-    public Button enqueuePatternButton;
-    public Button enqueueSecondPatternButton;
-    public GameObject startPatternSprite;
     private bool isMouseDownOnStartPattern = false;
-
-    private Queue<Vector3> moveQueue = new Queue<Vector3>();
+    private bool isProcessingMoveQueue = false;
     
+    private Queue<Vector3> moveQueue = new Queue<Vector3>();
+    private int moveQueueIndex = 0;
+
     private void Start()
     {
         enqueuePatternButton.onClick.AddListener(EnqueueMoves);
         enqueueSecondPatternButton.onClick.AddListener(EnqueueSecondPatternMoves);
     }
 
+    private bool shouldMove = false;
+
+    private float elapsedTime;
+    private float rotationElapsedTime;
+
+    private float movementTimer = 0f; // hareket süresini kontrol etmek için yeni bir değişken
+
+    [SerializeField] private float rotationSpeed = 60f;
+
+    private float movementProgress = 0f;
+
     private void Update()
     {
-        if (isMoving)
-        {
-            CaldronManager.Instance.spiral.transform.Rotate(Vector3.forward * 60 * Time.deltaTime);
-            return;
-        }
-
         if (Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -41,105 +45,66 @@ public class HexagonMover : MonoBehaviour
 
             if (hitCollider != null && hitCollider.gameObject == startPatternSprite.gameObject)
             {
-                isMouseDownOnStartPattern = true;
-                StartPatternMoves();
+                isMouseDownOnStartPattern = !isMouseDownOnStartPattern;
             }
         }
-
-        if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonDown(1))
         {
-            if (isMouseDownOnStartPattern)
-            {
-                isMouseDownOnStartPattern = false;
-                StopPatternMoves();
-            }
+            isMouseDownOnStartPattern = false;
         }
 
-        if (moveCommands.Count > 0)
+        if (isMouseDownOnStartPattern)
         {
-            Vector3 direction = moveCommands.Dequeue();
-            StartCoroutine(MoveToNextHexagon(direction));
+            CaldronManager.Instance.spiral.transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
         }
-        else
-        {
-            int horizontal = (int)Input.GetAxisRaw("Horizontal");
-            int vertical = (int)Input.GetAxisRaw("Vertical");
+    }
 
-            if (horizontal != 0 || vertical != 0)
+    private void FixedUpdate()
+    {
+        if (isMouseDownOnStartPattern && moveQueue.Count > 0)
+        {
+            movementTimer += Time.fixedDeltaTime;
+            movementProgress = movementTimer / moveDuration;
+
+            if (movementTimer >= moveDuration)
             {
-                Vector3 direction = new Vector3(horizontal, vertical, 0f).normalized;
-                StartCoroutine(MoveToNextHexagon(direction));
-                PrintCurrentHexagonColor();
+                Vector3 direction = moveQueue.ElementAt(moveQueueIndex % moveQueue.Count);
+                MoveToNextHexagon(direction);
+                moveQueueIndex++;
+                movementTimer = 0f;
             }
         }
     }
 
-
-    private IEnumerator MoveToNextHexagon(Vector3 direction)
+    private void MoveToNextHexagon(Vector3 direction)
     {
-        isMoving = true;
-
         Vector3 startPosition = transform.position;
         Vector3 targetPosition = startPosition + GetHexagonOffset(direction);
 
-        float elapsedTime = 0f;
-        while (elapsedTime < moveDuration)
+        movementProgress = Mathf.Clamp01(movementProgress);
+        transform.position = Vector3.Lerp(startPosition, targetPosition, movementProgress);
+
+        if (movementProgress >= 1f)
         {
-            elapsedTime += Time.deltaTime;
-            float t = elapsedTime / moveDuration;
-            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
-            yield return null;
+            PrintCurrentHexagonColor();
         }
-
-        transform.position = targetPosition;
-
-        PrintCurrentHexagonColor(); // Hareket işlemi tamamlandıktan sonra rengi yazdır
-
-        isMoving = false;
     }
 
 
+    
     private Vector3 GetHexagonOffset(Vector3 direction)
     {
-        Vector3 offset = Vector3.zero;
+        if (direction == Vector3.up) return new Vector3(0f, 0.55f, 0f);
+        if (direction == Vector3.down) return new Vector3(0f, -0.55f, 0f);
 
-        if (direction == Vector3.up) // Up
-        {
-            offset = new Vector3(0f, 0.55f, 0f);
-        }
-        else if (direction == Vector3.down) // Down
-        {
-            offset = new Vector3(0f, -0.55f, 0f);
-        }
-        else if (direction.x > 0) // Right
-        {
-            if (direction.y > 0) // Right-Up
-            {
-                offset = new Vector3(0.475f, 0.275f, 0f);
-            }
-            else // Right-Down
-            {
-                offset = new Vector3(0.475f, -0.275f, 0f);
-            }
-        }
-        else if (direction.x < 0) // Left
-        {
-            if (direction.y > 0) // Left-Up
-            {
-                offset = new Vector3(-0.475f, 0.275f, 0f);
-            }
-            else // Left-Down
-            {
-                offset = new Vector3(-0.475f, -0.275f, 0f);
-            }
-        }
-
-        return offset;
+        float x = direction.x > 0 ? 0.475f : -0.475f;
+        float y = direction.y > 0 ? 0.275f : -0.275f;
+        return new Vector3(x, y, 0f);
     }
     
     private void PrintCurrentHexagonColor()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,        Vector2.zero);
         if (hit.collider != null)
         {
             Hexagon hexagon = hit.collider.GetComponent<Hexagon>();
@@ -151,82 +116,41 @@ public class HexagonMover : MonoBehaviour
         }
     }
     
-    /////////////////////////Patterns/////////////////////////////////
-    ///
-    private IEnumerator ExecutePatternMoves(Vector3[] directions)
-    {
-        for (int i = 0; i < directions.Length; i++)
-        {
-            Vector3 direction = directions[i];
-            StartCoroutine(MoveToNextHexagon(direction));
-            yield return new WaitForSeconds(moveDuration);
-        }
-    }
-    
-    public void EnqueueMoves()
-    {
-        if (!isMoving)
-        {
-            Vector3[] patternDirections = {
-                new Vector3(-1, 1, 0),  // Left-Up
-                new Vector3(1, 1, 0),   // Right-Up
-                new Vector3(1, -1, 0),  // Right-Down
-                new Vector3(0, 1, 0),   // Up
-                new Vector3(-1, 1, 0)   // Left-Up
-            };
-
-            EnqueueDirections(patternDirections);
-        }
-    }
-    
-    public void EnqueueSecondPatternMoves()
-    {
-        if (!isMoving)
-        {
-            Vector3[] secondPatternDirections = {
-                new Vector3(1, -1, 0),  // Right-Down
-                new Vector3(1, 1, 0),   // Right-Up
-                new Vector3(0, -1, 0)   // Down
-            };
-
-            EnqueueDirections(secondPatternDirections);
-        }
-    }
-    
     private void EnqueueDirections(Vector3[] directions)
     {
-        for (int i = 0; i < directions.Length; i++)
+        foreach (Vector3 direction in directions)
         {
-            moveQueue.Enqueue(directions[i]);
+            moveQueue.Enqueue(direction);
         }
         PrintMoveQueue();
     }
 
-    public void StartPatternMoves()
+    public void EnqueueMoves()
     {
-        if (!isMoving && moveQueue.Count > 0)
-        {
-            StartCoroutine(ExecuteMoveQueue());
-        }
+        if (isMoving) return;
+
+        Vector3[] patternDirections = {
+            new Vector3(-1, 1, 0),  // Left-Up
+            new Vector3(1, 1, 0),   // Right-Up
+            new Vector3(1, -1, 0),  // Right-Down
+            new Vector3(0, 1, 0),   // Up
+            new Vector3(-1, 1, 0)   // Left-Up
+        };
+
+        EnqueueDirections(patternDirections);
     }
-
-    public void StopPatternMoves()
+    
+    public void EnqueueSecondPatternMoves()
     {
-        StopCoroutine(ExecuteMoveQueue());
-    }
+        if (isMoving) return;
 
-    private IEnumerator ExecuteMoveQueue()
-    {
-        isMoving = true;
+        Vector3[] secondPatternDirections = {
+            new Vector3(1, -1, 0),  // Right-Down
+            new Vector3(1, 1, 0),   // Right-Up
+            new Vector3(0, -1, 0)   // Down
+        };
 
-        while (moveQueue.Count > 0)
-        {
-            Vector3 direction = moveQueue.Dequeue();
-            StartCoroutine(MoveToNextHexagon(direction));
-            yield return new WaitForSeconds(moveDuration);
-        }
-
-        isMoving = false;
+        EnqueueDirections(secondPatternDirections);
     }
 
     private void PrintMoveQueue()
@@ -237,5 +161,4 @@ public class HexagonMover : MonoBehaviour
             Debug.Log(direction);
         }
     }
-
 }

2c74cdca36c2131dcdf25e00bc09a5bba375a808
emirYarici <e-yarici@hotmail.com>
2023-04-13 10:33:07 +0300

    dotween package

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,48 @@ public class ColorManager : MonoBehaviour
     public Button redButton;
     public Button greenButton;
     public Button blueButton;
-
+    public GameObject movingR;
+    public GameObject movingG;
+    public GameObject movingB;
     public void OnClickRedButton()
     {
-        CaldronManager.Instance.spiral.color = new Color(1, 51/255f, 51/255f,1);
+        Colorize(new Color(1, 51 / 255f, 51 / 255f, 1));
+        MoveToEmptySpot(movingR,redButton);
     }
     
     public void OnClickGreenButton()
     {
-        CaldronManager.Instance.spiral.color = new Color(51/255f, 1, 51/255f,1);
+        Colorize(new Color(51 / 255f, 1, 51 / 255f, 1));
+        MoveToEmptySpot(movingG, greenButton);
     }
     
     public void OnClickBlueButton()
     {
-        CaldronManager.Instance.spiral.color = new Color(51/255f, 51/255f,1, 1);
+        Colorize(new Color(51 / 255f, 51 / 255f, 1, 1));
+        MoveToEmptySpot(movingB, blueButton);
     }
+
+    public void MoveToEmptySpot(GameObject movingColor,Button button)
+    {
+        if(CaldronManager.Instance.lastHexagonPos != -1)
+        {
+            GameObject temp =  Instantiate(movingColor, button.gameObject.transform);
+            temp.transform.DOMove(CaldronManager.Instance.hexagonSpotsList[CaldronManager.Instance.lastHexagonPos].transform.position, 1f).OnComplete(() =>
+            {
+                CaldronManager.Instance.hexagonSpotsList[CaldronManager.Instance.lastHexagonPos].GetComponent<Image>().sprite = temp.GetComponent<Image>().sprite;
+                Destroy(temp.gameObject);
+            });
+            CaldronManager.Instance.lastHexagonPos++;
+
+            if (CaldronManager.Instance.lastHexagonPos > 2)
+            {
+                CaldronManager.Instance.lastHexagonPos = -1;
+            }
+        }
+    }
+    public void Colorize(Color color)
+    {
+        CaldronManager.Instance.spiral.color = color;
+    }
+
 }

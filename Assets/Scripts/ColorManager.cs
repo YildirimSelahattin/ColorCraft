1fcb7a5141d8baf9b80fe64fdb790e65cd5885d9
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-04-17 13:08:55 +0300

    .

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -37,11 +37,11 @@ public class ColorManager : MonoBehaviour
                     break;
                 case "green":
                     colorIndex = 1;
-                    colorButton.GetComponent<Image>().color = new Color(131/256f, 131/256f,256/256f);
+                    colorButton.GetComponent<Image>().color = new Color(110/256f, 218/256f,140/256f);
                     break;
                 case "blue":
                     colorIndex = 2;
-                    colorButton.GetComponent<Image>().color = new Color(110/256f, 218/256f,140/256f);
+                    colorButton.GetComponent<Image>().color = new Color(131/256f, 131/256f,256/256f);
                     break;
                 case "yellow":
                     colorIndex = 3;
